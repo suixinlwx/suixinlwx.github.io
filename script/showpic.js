c0d51cfa86b838e2fbfa303decb28cfa70474cf0
suixinlwx <38269851+suixinlwx@users.noreply.github.com>
2018-04-25 10:41:26 +0800

    Update showpic.js

--- script/showpic.js
+++ script/showpic.js
@@ -2,5 +2,8 @@ function showpic(whichpic){
   var source=whichpic.getAttribute("href");
   var placeholder=document.getElementById("placeholder");
   placeholder.setAttribute("src",source);
+  var txet=whichpic.getAttribute("title");
+  var description=document.getElementByld("description");
+  description.firstChild.nodeValue=text;
 }
   
